fdf42657a3a659b909c8c5fff01d131698463cc9
zabronax <zabronax@gmail.com>
2025-11-17 11:22:16 +0100

    add examples of various built in types

--- Program.cs
+++ Program.cs
@@ -5,3 +5,13 @@ var espen = new Person("Espen");
 lars.SayHello();
 per.SayHello();
 espen.SayHello();
+
+// Various inbuilt types, these are objects/classes
+// they where just defined by someone else
+// See all inbuilt types in the C# language
+// .NET includes even more primitive types
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
+var textString = "Hello World";
+var integer = 10;
+var floatingPointNumber = 10.43;
+var boolean = true;
