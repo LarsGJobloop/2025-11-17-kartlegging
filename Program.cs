2fe7570db7357c8ce23e8e35e6bc280f83e6b35e
zabronax <zabronax@gmail.com>
2025-11-17 09:45:35 +0100

    add fundamental programming concepts

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// This is a comment
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Testing testing");
+Console.WriteLine("Hello hello!");
+
+var a = 10;
+var b = 32;
+var c = a + b;
+Console.WriteLine(c);
+
+if (c > 40)
+{
+  Console.WriteLine("C is larger than 40");
+} else
+{
+  Console.WriteLine("C is less than 40");
+}
+
+var count = 0;
+
+while (count < 10)
+{
+  Console.WriteLine("Count is");
+  Console.WriteLine(count);
+  count++;
+}
