70fb48b34a6156318da1f34ddb9ff3307b931ab7
zabronax <zabronax@gmail.com>
2025-11-18 13:22:29 +0100

    clean up example

--- Program.cs
+++ Program.cs
@@ -1,17 +1,2 @@
-﻿var lars = new Person("Lars Gunnar");
-var per = new Person("Per");
-var espen = new Person("Espen");
+﻿Console.WriteLine("Starting Program");
 
-lars.SayHello();
-per.SayHello();
-espen.SayHello();
-
-// Various inbuilt types, these are objects/classes
-// they where just defined by someone else
-// See all inbuilt types in the C# language
-// .NET includes even more primitive types
-// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
-var textString = "Hello World";
-var integer = 10;
-var floatingPointNumber = 10.43;
-var boolean = true;
